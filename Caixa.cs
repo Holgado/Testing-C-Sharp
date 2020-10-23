d36b2fb1802af100cddc49ce11bd1af083bcf13b
Marcelo Holgado <holgadomarcelo184@gmail.com>
2020-10-23 10:41:30 -0300

    Add files via upload

---
+++ Caixa.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Caixa
+    {
+        private static int valorTot;
+        private static int[] cedulasNoCaixa = new int[7];
+
+        public int getValTot()
+        {
+            return valorTot;
+        }
+        private void setValTot(int val)
+        {
+            valorTot += val;
+        }
+        public int[] getCedulasDisp()
+        {
+            return cedulasNoCaixa;
+        }
+        public int CedulasNoCaixa
+        {
+            set
+            {                
+                if (value == 2)
+                {
+                    cedulasNoCaixa[0] = value;
+                    setValTot(value);
+                }
+                else if (value == 5)
+                {
+                    cedulasNoCaixa[1] = value;
+                    setValTot(value);
+                }
+                else if (value == 10)
+                {
+                    cedulasNoCaixa[2] = value;
+                    setValTot(value);
+                }
+                else if (value == 20)
+                {
+                    cedulasNoCaixa[3] = value;
+                    setValTot(value);
+                }
+                else if (value == 50)
+                {
+                    cedulasNoCaixa[4] = value;
+                    setValTot(value);
+                }
+                else if (value == 100)
+                {
+                    cedulasNoCaixa[5] = value;
+                    setValTot(value);
+                }
+                else if (value == 200)
+                {
+                    cedulasNoCaixa[6] = value;
+                    setValTot(value);
+                }
+            }
+        }
+        public void contaNotas()
+        {
+            Console.WriteLine("Notas de R$2 = " + cedulasNoCaixa[0] / 2);
+            Console.WriteLine("Notas de R$5 = " + cedulasNoCaixa[1] / 5);
+            Console.WriteLine("Notas de R$10 = " + cedulasNoCaixa[2] / 10);
+            Console.WriteLine("Notas de R$20 = " + cedulasNoCaixa[3] / 20);
+            Console.WriteLine("Notas de R$50 = " + cedulasNoCaixa[4] / 50);
+            Console.WriteLine("Notas de R$100 = " + cedulasNoCaixa[5] / 100);
+            Console.WriteLine("Notas de R$200 = " + cedulasNoCaixa[6] / 200);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Array[7] 
+            // 50    em notas 2 
+            // 125   em notas 5
+            // 80    em notas 10
+            // 20    em notas 20
+            // 50    em notas 50
+            // 100   em notas 100
+            // 200   em notas 200
+
+            Caixa caixa1 = new Caixa();
+            int opcao = 0;
+            while (opcao != 4) {
+                Console.WriteLine("CAIXA REGISTRADORA");
+                Console.WriteLine(" 1) Receber pagamento");
+                Console.WriteLine(" 2) Consultar valor no caixa");
+                Console.WriteLine(" 3) Mostrar qauntidade de cédulas");
+                Console.WriteLine(" 4) Sair");
+                opcao = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("-----------------------------------\n");
+                if (opcao == 1)
+                {
+                    Console.Write("Digite o valor recebido: ");
+                    caixa1.CedulasNoCaixa = Convert.ToInt32(Console.ReadLine());
+                }
+                else if (opcao == 2)
+                {
+                    Console.WriteLine("Valor total no caixa R$" + caixa1.getValTot());
+                }
+                else if (opcao == 3)
+                {
+                    Console.WriteLine("Quantidade de cada tipo de cédula: ");
+                    caixa1.contaNotas();
+                }
+            }
+            Console.WriteLine("Finalizando ...");
+            Console.ReadLine();
+        }
+    }
+}
